9eb44522095fb6baef845f0044eb0410a074ea63
Raymond Jaekwang Lee <rayjaelee@gmail.com>
2022-03-07 16:23:50 +0900

    added new files and updated feed mechanism

---
+++ glostock/src/main/java/com/glostock/apiservice/PolygonAPIService.java
@@ -0,0 +1,104 @@
+package com.glostock.apiservice;
+
+import com.glostock.model.CompanyVO;
+import com.glostock.model.NewsVO;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class PolygonAPIService {
+
+    private static String input_ticker = "";
+
+    static final String USER_AGENT = "Mozilla/5.0";
+
+    public static ArrayList<NewsVO> getThreeNews(String company_ticker) throws IOException {
+        ArrayList<NewsVO> three_recent_news = new ArrayList<>();
+
+        String NEWSAPI_URL = "https://api.polygon.io/v2/reference/news?ticker=" + company_ticker + "&apiKey=Q2mEmcBtNaeo2pmA5WgKU0h7rVYvFrJY";
+
+        URL news_obj = new URL(NEWSAPI_URL);
+
+        HttpURLConnection newsURLConnection = (HttpURLConnection) news_obj.openConnection();
+        newsURLConnection.setRequestMethod("GET");
+        newsURLConnection.setRequestProperty("User-Agent", USER_AGENT);
+
+        if (newsURLConnection.getResponseCode() == newsURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(newsURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer newsresponse = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                newsresponse.append(inputLine);
+            }
+            in.close();
+
+            JSONObject jObject = new JSONObject(newsresponse.toString());
+
+            JSONArray newsArray = jObject.getJSONArray("results");
+
+            for (int i = 0; i < 3; i++) {
+                NewsVO vo = new NewsVO();
+                vo.setPublisher_name(newsArray.getJSONObject(i).getJSONObject("publisher").getString("name"));
+                vo.setArticle_author(newsArray.getJSONObject(i).getString("author"));
+                vo.setArticle_description(newsArray.getJSONObject(i).getString("description"));
+                vo.setArticle_title(newsArray.getJSONObject(i).getString("title"));
+                vo.setArticle_url(newsArray.getJSONObject(i).getString("article_url"));
+                vo.setArticle_image_url(newsArray.getJSONObject(i).getString("image_url"));
+                vo.setPublisher_logo_url(newsArray.getJSONObject(i).getJSONObject("publisher").getString("logo_url"));
+                vo.setPublisher_icon_url(newsArray.getJSONObject(i).getJSONObject("publisher").getString("favicon_url"));
+                vo.setArticle_timestamp(newsArray.getJSONObject(i).getString("published_utc"));
+                three_recent_news.add(vo);
+            }
+
+        } else {
+            System.out.println("GET request not worked");
+        }
+
+        return three_recent_news;
+    }
+
+    public static CompanyVO getCompanyProfile(String company_ticker) throws IOException {
+        CompanyVO profile = new CompanyVO();
+        String COMPANY_PROFILE_URL = "https://api.polygon.io/v3/reference/tickers/" + company_ticker + "?apiKey=Q2mEmcBtNaeo2pmA5WgKU0h7rVYvFrJY";
+
+
+        URL profileRequestObject = new URL(COMPANY_PROFILE_URL);
+        HttpURLConnection profileURLConnection = (HttpURLConnection) profileRequestObject.openConnection();
+        profileURLConnection.setRequestMethod("GET");
+        profileURLConnection.setRequestProperty("User-Agent", USER_AGENT);
+
+        if (profileURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(profileURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer companyProfileResponse = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                companyProfileResponse.append(inputLine);
+            }
+            in.close();
+
+            JSONObject jObject = new JSONObject(companyProfileResponse.toString());
+
+            JSONObject companyProfileObject = jObject.getJSONObject("results");
+            profile.setCompany_ticker(companyProfileObject.getString("ticker"));
+            profile.setCompany_name(companyProfileObject.getString("name"));
+            profile.setCompany_description(companyProfileObject.getString("description"));
+            profile.setCompany_website_url(companyProfileObject.getString("homepage_url"));
+            profile.setCompany_currency(companyProfileObject.getString("currency_name"));
+            profile.setCompany_logo_url(companyProfileObject.getJSONObject("branding").getString("logo_url"));
+            profile.setCompany_description_ko(PapagoAPIService.translateENtoKR(profile.getCompany_description()));
+
+        } else {
+            System.out.println("GET request not worked");
+        }
+        return profile;
+    }
+
+}
--- glostock/src/main/webapp/WEB-INF/views/user/follow.jsp
+++ glostock/src/main/webapp/WEB-INF/views/user/follow.jsp
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
     <title>My Following | Glo Stock</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -49,7 +47,6 @@
         }
 
     </style>
-
 
 </head>
 <body>
