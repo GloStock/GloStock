57e3d5424c3262bc67ec69765e52421100fd66de
ywoo904 <ywoo904@gmail.com>
2022-03-11 10:40:58 +0900

    .
    .

--- glostock/src/main/java/com/glostock/controller/UserServiceController.java
+++ glostock/src/main/java/com/glostock/controller/UserServiceController.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
 
 import com.glostock.model.FollowVO;
+import com.glostock.model.PortfolioVO;
 import com.glostock.model.StockVO;
 import com.glostock.service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
