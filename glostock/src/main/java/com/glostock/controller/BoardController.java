97072c2764935e32fee0f0fe90c8fc8131f0599c
ywoo904 <ywoo904@gmail.com>
2022-03-11 14:36:02 +0900

    .
    .

--- glostock/src/main/java/com/glostock/controller/BoardController.java
+++ glostock/src/main/java/com/glostock/controller/BoardController.java
@@ -1,7 +1,15 @@
 package com.glostock.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
+@RequestMapping
 public class BoardController {
 
+	
+	
+	
+	
+	
 }
